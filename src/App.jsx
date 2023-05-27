a3c76e99c745d59393cdf73d63cc493a212fede3
Abdullah Bin Rashid <71758955+abdullah19191@users.noreply.github.com>
2023-05-27 17:42:01 +0500

    Chnages added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { Hero } from './components'
-
+import {heroapi} from '../src/Data/data.js'
 const App = () => {
   return (
     <>
     <main>
-      <Hero/>
+      <Hero  heroapi={heroapi}/>
     </main>
     </>
   )
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,6 +2,18 @@ import React from 'react'
 
 export const Hero = () => {
   return (
-    <div>Hero</div>
+    <div>
+        <div className="">Gradient</div>
+        <div className="">
+            <div className="">
+                <h1></h1>
+                <h1></h1>
+                <button></button>
+                <div></div>
+                <div></div>
+            </div>
+            
+        </div>
+    </div>
   )
 }
