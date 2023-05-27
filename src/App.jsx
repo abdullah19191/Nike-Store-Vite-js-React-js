49a8e630ea65fb887faa2067cb6fa84146858a53
Abdullah Bin Rashid <71758955+abdullah19191@users.noreply.github.com>
2023-05-27 17:27:48 +0500

    added components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { Hero } from './components'
 
 const App = () => {
   return (
-    <div className=' text-cyan-200'>Nike App Ho sala</div>
+    <>
+    <main>
+      <Hero/>
+    </main>
+    </>
   )
 }
 
