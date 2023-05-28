f072275957111507688ede423a1f4f3d79b3d793
Abdullah Bin Rashid <71758955+abdullah19191@users.noreply.github.com>
2023-05-28 15:31:59 +0500

    Aer

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,19 +1,28 @@
+/* eslint-disable react/prop-types */
 import React from 'react'
-
-export const Hero = () => {
+import '../index.css'
+ const Hero = ({heroapi : {title,subtitle,btntext,img,sociallinks,videos}}) => {
   return (
-    <div>
-        <div className="">Gradient</div>
-        <div className="">
-            <div className="">
-                <h1></h1>
-                <h1></h1>
-                <button></button>
-                <div></div>
-                <div></div>
+    <div className=' relative h-auto w-auto flex flex-col '>
+        <div className='bg-theme clip-path h-[85vh] lg:h-[75vh] md:h-[65vh] sm:h-[55vh] w-auto absolute top-0 left-0 right-0 opacity-100 z-100'></div>
+        <div className=" relative opacity-100 z-20 justify-items-center grid items-center nike-container">
+            <div className=" grid items-center justify-items-center mt-28 md:mt-24">
+                <h1 className=' text-6xl lg:text-5xl md:text-4xl sm:text-3xl xsm:text-2xl font-extrabold filter drop-shadow-sm  text-slate-200 ' >{title}</h1>
+                <h1 className=' text-6xl lg:text-5xl md:text-4xl sm:text-3xl xsm:text-2xl font-extrabold filter drop-shadow-sm  text-slate-200 '>{subtitle}</h1>
+                <button className='button-theme font-medium bg-slate-200 shadow-slate-200 rounded-xl my-5'>
+                    {btntext}
+                </button>
+                <div className=''></div>
+                <div className=''></div>
             </div>
-            
+            <div className="">
+                <img 
+                className=' -rotate-[25deg] hover:rotate-0 w-auto h-[45vh] lg:h-[35vh] md:h-[31vh] sm:h-[21vh] xsm:h-[19vh] transitions-theme' 
+                src={img} alt="" />
+            </div>
+
         </div>
     </div>
   )
 }
+export default Hero
